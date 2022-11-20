ebee5cbf82346f27e36eac74d02f677bbf41cad0
EvergreenHZ <huaizhixtp@163.com>
2022-11-20 17:30:31 +0800

    use checkbox

--- event_tracking.py
+++ event_tracking.py
@@ -6,20 +6,21 @@ from datetime import datetime
 import pymysql
 
 db = None
+VERSIONS = ["v13.7", "v15.6"]
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return render_template('index.html', records=[], sql_statement="")
+        return render_template('index.html', records=[], versions=VERSIONS, sql_statement="")
     else:
         data = json.loads(request.data)
         print(data)
         terminal_type = data['terminal_type']
         dot_key = data['dot_key']
-        start_version = data['start_version']
-        end_version = data['end_version']
+        versions = data['versions']
+        print(versions)
         start_time = None
         if data['start_time']:
             start_time = datetime.timestamp(datetime.fromisoformat(data['start_time']))
@@ -29,17 +30,17 @@ def index():
             end_time = datetime.timestamp(datetime.fromisoformat(data['end_time']))
         print(end_time)
 
-        sql_statement = 'SELECT * FROM db_dot_record\n'
-        condition = " terminal_type: {0}, dot_key: {1}, start_time: {2}, end_time: {3}, start_version: {4}, end_version: {5}"
-        condition = condition.format(terminal_type, dot_key, data['start_time'], data['end_time'], start_version, end_version)
+        sql_statement = 'SELECT * FROM db_dot_record'
+        condition = " terminal_type: {0}, dot_key: {1}, start_time: {2}, end_time: {3}"
+        condition = condition.format(terminal_type, dot_key, data['start_time'], data['end_time'])
 
         records = [
-            (u'红楼梦', u'曹雪芹', 200),
-            (u'水浒传', u'施耐庵', 100),
-            (u'三国演义', u'罗贯中', 120),
-            (u'西游记', u'吴承恩', 230)
         ]
-        return render_template('index.html', records=records, sql_statement=sql_statement, condition=condition)
+        return render_template('index.html',
+                               records=records,
+                               versions=VERSIONS,
+                               sql_statement=sql_statement,
+                               condition=condition)
 
 if __name__ == '__main__':
     '''
--- static/js/event_tracking.js
+++ static/js/event_tracking.js
@@ -4,25 +4,17 @@ $("#btn_query").click(function() {
     let dot_key = $('#dot_key').val();
     let start_time = $('#start_time').val();
     let end_time = $('#end_time').val();
-    let start_version = $('#start_version').val();
-    let end_version = $('#end_version').val();
-    //console.log(terminal_type);
-    //console.log(start_time);
-    //console.log(end_time);
-    //console.log(start_version);
-    //console.log(end_version);
+    let versions = [];
+    $("input:checkbox:checked").each(function(){
+        versions.push(this.id);
+    });
     let data = {
         'terminal_type' : terminal_type,
         'dot_key'       : dot_key,
         'start_time'    : start_time,
         'end_time'      : end_time,
-        'start_version' : start_version,
-        'end_version'   : end_version
+        'versions'      : versions
     };
-    //console.log(data);
-    //console.log(data);
-    //console.log(image_vs_novel);
-    //window.location.href = novel_path;
 
     $.ajax({
         url: '/',
@@ -30,15 +22,17 @@ $("#btn_query").click(function() {
         contentType: 'application/json',
         data: JSON.stringify(data),
         success: function(result) {
-            //console.log(result);
+            console.log(result);
             document.open();
             document.write(result);
             document.close();
             $('#terminal_type').val(terminal_type)
             $("#start_time").val(start_time)
             $("#end_time").val(end_time)
-            $("#start_version").val(start_version)
-            $("#end_version").val(end_version)
+
+            for (const version of versions) {
+                $('#' + version.replace('.', '\\.')).prop("checked", true);
+            }
         }
     });
 });
